b57415dcc19e781a7cdf58c016e25b6e6a038a29
Rob Sanheim <rsanheim@gmail.com>
2008-07-10 23:43:44 -0400

    better

--- test/spec_focused.rb
+++ test/spec_focused.rb
@@ -1,4 +1,4 @@
-require 'helper'
+require File.expand_path(File.join(File.dirname(__FILE__), *%w[helper]))
 
 describe "focused methods" do
   it "context methods are extended on to the test case" do
